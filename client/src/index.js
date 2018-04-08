18a29ca8f8e1fc8e1342dd8ebc857e5384636ac4
Arcady <C:\Users\user\AppData\Roaming\The Bat! Pwd>
2018-04-08 14:05:00 +0300

    transaction lib implementation

--- client/src/index.js
+++ client/src/index.js
@@ -17,22 +17,11 @@ class Wallet {
   }
 
   async formTransaction() {
-    await this.getTx();
-    // await this.utxo.forEach(utxo => {
-    //   this.transaction = this.transaction.from(utxo);
-    // });
-    const utxo = {
-        txId: "47ab7e63cd24aaa91bfa20f193e2d3bed35839a08eb1062479f49de39a881503",
-        outputIndex: 0,
-        address: "mmZW3aHQ5PULPv7G3Gwb8t3SG3GLPEwbbY",
-        script: "DUP HASH160 PUSHDATA(20)[424c172a419e4bd1773649bae2ffac8fa712651e] EQUALVERIFY CHECKSIG",
-        amount: 82410240
-    }
-    this.transaction.from(utxo);
-    await this.transaction.to(this.destinationAddress, this.amount).sign(this.pKey);
-    await this.transaction.change(this.address);
-    // console.log(this.transaction.serialize({disableIsFullySigned: true}));
-    console.log(this.transaction.serialize());
+    await this.getTx();    
+    this.transaction.from(this.utxo, this.address);
+    this.transaction.to(this.destinationAddress, this.amount).sign(this.pKey);
+    this.transaction.change(this.address);
+    return this.transaction.serialize({ disableIsFullySigned: true });
   }
 
   async getTx() {
@@ -40,12 +29,12 @@ class Wallet {
       .then(response => response.json())
       .then(transactions => transactions)
       .catch(err => console.err(err));
-    transactions.forEach(t => {      
+    transactions.forEach(t => {
       this.utxo.push({
         txId: t.txid,
         outputIndex: t.vout,
         address: t.address,
-        script: t.scriptPubKey,
+        script: bitcore.Script.buildPublicKeyHashOut(this.address).toString(),
         satoshis: t.satoshis
       });
     });
@@ -53,13 +42,16 @@ class Wallet {
   }
 }
 
-window.onload = () => {
-  const destinationAddress = "mmZW3aHQ5PULPv7G3Gwb8t3SG3GLPEwbbY";
-  const amount = 5000;
-  const wallet = new Wallet({
-    pKey: "cRJLFES5NSkLAXYP9CDviTpYw4o6M2nMw1SxKChuXLjiPt7zAvut",
-    destinationAddress,
-    amount
-  });
-  wallet.formTransaction();
-};
+// window.onload = () => {
+//   const destinationAddress = "mmZW3aHQ5PULPv7G3Gwb8t3SG3GLPEwbbY";
+//   const amount = 5000;
+//   const wallet = new Wallet({
+//     pKey: "cVJYN2BKqEHAKZQNZUocVYJT2Aee5TTTo8Ke7Yu5Qno6jRmEfsUu",
+//     destinationAddress,
+//     amount
+//   });
+//   wallet.formTransaction();
+// };
+
+window.Wallet = Wallet;
+export default Wallet;
